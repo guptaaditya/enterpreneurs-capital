b2fc88bd912d8abd7d963775ddf0a9d70b47052d
Aditya Gupta <guptaaditya24@gmail.com>
2019-11-23 03:39:27 +0530

    build check

---
+++ precache-manifest.4b756801c7691aabb79047a7073e1dca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a7ca0ef849483f4ba37628d5b7421d8",
+    "url": "/enterpreneurs-capital/index.html"
+  },
+  {
+    "revision": "60ff93e73281c6481cfd",
+    "url": "/enterpreneurs-capital/static/css/main.31f3a5ab.chunk.css"
+  },
+  {
+    "revision": "1e3501772360c6f23f09",
+    "url": "/enterpreneurs-capital/static/js/2.71aef25a.chunk.js"
+  },
+  {
+    "revision": "60ff93e73281c6481cfd",
+    "url": "/enterpreneurs-capital/static/js/main.706e9cb3.chunk.js"
+  },
+  {
+    "revision": "7df6f622fa20c59c7860",
+    "url": "/enterpreneurs-capital/static/js/runtime-main.6d572170.js"
+  }
+]);
